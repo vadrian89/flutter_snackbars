d2b9cafa92abb0ec809eb7be459db2d2bc3b1a77
Adrian Verban <verbanady@gmail.com>
2022-11-15 13:50:12 +0200

    Updated

--- lib/flutter_snackbars.dart
+++ lib/flutter_snackbars.dart
@@ -1,1 +1,3 @@
 library flutter_snackbars;
+
+export 'src/enhanced_snackbar.dart' show EnhancedSnackBar;
--- lib/src/enhanced_snackbar.dart
+++ lib/src/enhanced_snackbar.dart
@@ -4,51 +4,18 @@ import 'package:flutter/material.dart';
 
 class EnhancedSnackBar extends SnackBar {
   const EnhancedSnackBar({
-    Key? key,
-    required Widget content,
-
-    /// The duration, in seconds, which the snackbar will be shown.
-    Duration duration = const Duration(seconds: 4),
-
-    /// The value [SnackBar.width].
-    double? width,
-
-    /// The color of foreground widgets, such as texts.
-    Color? foregroundColor,
-
-    /// The value [SnackBar.backgroundColor].
-    Color? backgroundColor,
-
-    /// The value [SnackBar.elevation].
-    double? elevation,
-
-    /// The value [SnackBar.margin].
-    EdgeInsetsGeometry? margin,
-
-    /// The value [SnackBar.padding].
-    EdgeInsetsGeometry? padding,
-
-    /// The value [SnackBar.shape].
-    ShapeBorder? shape,
-
-    /// The value [SnackBar.action].
-    SnackBarAction? action,
-
-    /// The value [SnackBar.behavior].
-    SnackBarBehavior? behavior,
-  }) : super(
-          key: key,
-          content: content,
-          behavior: behavior,
-          width: width,
-          backgroundColor: backgroundColor,
-          elevation: elevation,
-          margin: margin,
-          shape: shape,
-          action: action,
-          duration: duration,
-          padding: padding,
-        );
+    super.key,
+    required super.content,
+    super.duration = const Duration(seconds: 4),
+    super.width,
+    super.backgroundColor,
+    super.elevation,
+    super.margin,
+    super.padding,
+    super.shape,
+    super.action,
+    super.behavior,
+  });
 
   /// Display the snackbar on the nearest [Scaffold].
   ///
